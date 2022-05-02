5d5a08588ee8f6e005a652dd81a66b27b5a83c06
Justin Moore <jtnmoore@gmail.com>
2022-05-02 11:01:28 -0400

    Update main.py
    Adding call to create list function

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import actions as a
+
 def main():
     print("Press 'Q' to quit or select from  the following options:")
     #user input to determine action
     while True:
         action = input("'C' to create new list\n'V' to view list\n'A' to add to list\n'D' to delete from list\n'E' To edit item in list: ")
         #If user enters Q or q quit program
-        if action.lower() == 'q':
+        if action.lower() == 'c':
+            a.create_list(input("Please enter a list name: "))
+        elif action.lower() == 'q':
             return print("Thank you!")
 
 
