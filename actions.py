777061dad7c01e829312abaf13cab4989db2358f
Justin Moore <jtnmoore@gmail.com>
2022-05-02 22:29:03 -0400

    updates to actions
    updated logic and got rid of global lists

--- actions.py
+++ actions.py
@@ -1,6 +1,3 @@
-Stores = ["Target", "Trader Joes", "Walmart", "Other"]
-Sections = ["Appliances", "Automotive", "Clothing", "Deli", "Electronics", "Home", "Produce", "Toys", "Video Games", "Other", "Return"]
-
 from items import Item
 
 def create_list(list_name):
@@ -16,25 +13,21 @@ def add_to(list_name):
     list_name = []
     #continue looping until the user quits
     while True:
-        print_list(Stores)
         #user input for store name
-        store_int = input("Enter the number for the store you want to make a list for or 'Q' to quit: ")
-        if store_int.lower() == 'q':
+        store = input("Enter the name for the store you want to make a list for or 'R' to return: ")
+        if store.lower() == "r":
             #end function and print out list created
             print_list(list_name)
             return
         #store name value
-        store = Stores[int(store_int) - 1]
-        section_int = 0
         #loop while section is not return
-        while section_int != 11:
-            print_list(Sections)
+        section = ""
+        while section.lower() != "r":
             #user input for sections
-            section_int = int(input("\nEnter the number for the section the item can be found in from the above list or '11' to return to Store list: "))
-            section = Sections[section_int - 1]
+            section = input("Enter the name for the section the item can be found in or 'R' to return to Store list: ")
             name = ""
             #if not return continue adding items
-            if section_int != 11:
+            if section != "r":
                 while name.lower() != "r":
                     #user input for item names
                     name = input("Enter the items you would like to add to the list or type 'R' to return to Section list: ")
@@ -45,11 +38,5 @@ def add_to(list_name):
                     
         
 def print_list(list):
-    if list == Stores or list == Sections:
-        n = 1
-        for s in list:
-            print(str(n) + ". " + s + " -- ", end = '')
-            n += 1
-    else:
-        for i in list:
-            i.list_item()
+    for i in list:
+        i.list_item()
